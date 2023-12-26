84cc73a44174e2d4b7d55bf3172cf61801a03ec4
Mohammad Jabir ☑️ <79660697+jabir7860@users.noreply.github.com>
2023-12-26 15:21:29 +0530

    Create Notes.py

---
+++ Notes.py
@@ -0,0 +1,10 @@
+#Module 1 Introduction and Regression. 
+#Module 2 Classification 
+#Module 3 Culstering
+#Module 4 Final Project 
+#--------------------------------# 
+#Machine Learning Algorithms 
+""" Regression : 1) Simple Linear Regression 2) Multiple Linear regression 3)Regression Trees 
+    Classification : 1) Logistic Regression 2) KNN 3) SVM 4) Multiclass Prediction 5)Decision Trees
+    Clustering : 1)K - means
+""" 
