26e92573fad5fe81621f0e0c02f6bbbce82ee1c5
Mohammad Jabir ☑️ <79660697+jabir7860@users.noreply.github.com>
2023-12-26 15:40:30 +0530

    Update Notes.py

--- Notes.py
+++ Notes.py
@@ -2,9 +2,31 @@
 #Module 2 Classification 
 #Module 3 Culstering
 #Module 4 Final Project 
-#--------------------------------# 
+################################################################################################################## 
 #Machine Learning Algorithms 
 """ Regression : 1) Simple Linear Regression 2) Multiple Linear regression 3)Regression Trees 
     Classification : 1) Logistic Regression 2) KNN 3) SVM 4) Multiclass Prediction 5)Decision Trees
     Clustering : 1)K - means
 """ 
+
+"""
+    Example : Prime,Netflix Sujjestions,Bank Loan,Prediction.
+"""
+####################################################################################################################
+"""
+            MAJOR MACHINE LEARNING TECHNIQUES 
+1.REGRESSION / ESTIMATION : a) Predicting Continuous Values.  ex:CO2 emmission from vehicales 
+2.Classification : a)Predication item/Categroy. ex:cancer/not a cancer.
+3.Clustering : Finding structures of data. #Grouping Similar Data sets ex:similar patients/customers.
+4.Associatinos : Co-Occuring 
+5.anomaly detection : credicat card frad 
+6.squence mining : next recommendation on YT.
+7. Dimensioin Reduction : Reducing the size of data. 
+8.Recommendation systems : Recommendtion of itenms.
+
+"""
+######################################################################################################################
+DEEPLEARNING IS A SUBSET OF MACHINE LEARNING
+######################################################################################################################
+
+
